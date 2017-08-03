6faf5de74d386514181a61d603bc411e96ced6cf
Sarah Shuey <sarah.shuey@me.com>
2017-08-03 17:01:39 -0500

    eval

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-
 let numbs = document.querySelectorAll('.numb');
-numbs.forEach(function(n) {n.addEventListener('click', function (){console.log(n.value);
-})
+numbs.forEach(function(n) {
+  n.addEventListener('click', function() {
+    document.querySelector('.display').innerHTML += n.value
+  })
 })
 let opts = document.querySelectorAll('.opt');
-opts.forEach(function(o){o.addEventListener('click',
-function(){console.log(o.value);
-})
+opts.forEach(function(o) {
+  o.addEventListener('click',
+    function(event) {
+      // console.log(event.path[0].textContent)
+      if (event.path[0].textContent !== '='){
+        document.querySelector('.display').innerHTML += o.value
+      }
+      else {
+        let result = eval(document.querySelector('.display').innerHTML);
+        document.querySelector('.display').innerHTML=result
+        // console.log(result);
+      }
+    })
 })
