df22eff62c29978fc1f4cc169cbf4acc8279aece
Sarah Shuey <sarah.shuey@me.com>
2017-08-03 15:20:43 -0500

    eventlistener

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 
+let numbs = document.querySelectorAll('.numb');
+numbs.forEach(function(n) {n.addEventListener('click', function (){console.log(n.value);
+})
+})
+let opts = document.querySelectorAll('.opt');
+opts.forEach(function(o){o.addEventListener('click',
+function(){console.log(o.value);
+})
+})
